902e11ac9ac92f661ccf53b5c1ba99ca82a8e89b
Thomas Butler <rhom6us@users.noreply.github.com>
2019-10-24 15:41:56 -0400

    remove 'more changes'

--- RealDebrid.ts
+++ RealDebrid.ts
@@ -5,8 +5,6 @@ import { TorrentId, LinkInfo } from './types';
 import fetch from 'node-fetch';
 import {alert} from '../utils';
 
-// more changes
-
 type fu = ReturnType<typeof fetch>;
 // another chage
 export class RealDebrid {
